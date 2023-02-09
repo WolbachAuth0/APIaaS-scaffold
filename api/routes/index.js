5c3aa0264bfc41376291369b94d67f688f75be98
WolbachAuth0 <aaron.wolbach@okta.com>
2023-02-09 12:00:20 -0500

    add /token endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
-const path = require('path')
 const responseFormatter = require('../middleware/responseFormatter')
+const errorHandler = require('./../models/ErrorHandler')
+const http = require('axios')
+// const path = require('path')
 
 module.exports = router
 
@@ -14,16 +16,46 @@ router
       const json = responseFormatter(req, res, { status, message, data })
       res.status(status).json(json)
     } catch (error) {
-      const status = 500
-      const message = error.message
-      const data = error
-      const json = responseFormatter(req, res, { status, message, data })
-      res.status(status).json(json)
+      console.log(error)
+      const payload = errorHandler(error)
+      const json = responseFormatter(req, res, payload)
+      res.status(payload.status).json(json)
+    }
+  })
+
+router
+  .route('/token')
+  .post(async (req, res, next) => {
+    try {
+      const request = {
+        method: 'post',
+        url: `https://${process.env.VUE_APP_DOMAIN}/oauth/token`,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Accept': 'application/json'
+        },
+        data: new URLSearchParams({
+          client_id: req.body.client_id,
+          client_secret: req.body.client_secret,
+          audience: process.env.VUE_APP_AUTH0_AUDIENCE,
+          grant_type: 'client_credentials'
+        })
+      }
+      const response = await http(request)
+      console.log(response.data)
+
+      res.status(200).json(response.data)
+    } catch (error) {
+      console.log(error)
+      const payload = errorHandler(error)
+      const json = responseFormatter(req, res, payload)
+      res.status(payload.status).json(json)
     }
   })
 
 /**
   This endpoint has to be here to redirect invitation links to the signup screen.
+  This is only relevant when using the Auth0 Organizations feature.
   
   http://localhost:8081/api/login?invitation=W6yRtawKU1wmi9vY9Y0GbjGSpEQErLvq&organization=org_poSk5O5ljabdHiKV&organization_name=okta
 */
