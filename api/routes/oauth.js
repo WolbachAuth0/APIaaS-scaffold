611ce2196b93baec37150a393e033fac8193e890
WolbachAuth0 <aaron.wolbach@okta.com>
2023-05-01 20:31:44 -0400

    remove comments

--- api/routes/oauth.js
+++ api/routes/oauth.js
@@ -3,9 +3,6 @@ const controller = require('./../controllers/oauth')
 
 module.exports = router
 
-/**
- * TODO: This endpoint should implement a REDIS cache to cache the requested tokens.
- */
 router
   .route('/token')
   .post(
